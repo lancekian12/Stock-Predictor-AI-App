ae0982c8de0eed3797e494823d0fb24b24ae22b3
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-31 16:39:20 +0800

    small practice for promp ai

--- practice/index.js
+++ practice/index.js
@@ -1,3 +1,28 @@
+// import OpenAI from "openai";
+
+// const openai = new OpenAI({
+//   dangerouslyAllowBrowser: true,
+// });
+
+// const messages = [
+//   {
+//     role: "system",
+//     content:
+//       "You are a rap genius. When given a topic, create a five-line rap about that topic.",
+//   },
+//   {
+//     role: "user",
+//     content: "Television",
+//   },
+// ];
+
+// const response = await openai.chat.completions.create({
+//   model: "gpt-3.5 turbo",
+//   messages: messages,
+// });
+
+// console.log(response.choices[0].message.content);
+
 import OpenAI from "openai";
 
 const openai = new OpenAI({
@@ -8,16 +33,16 @@ const messages = [
   {
     role: "system",
     content:
-      "You are a rap genius. When given a topic, create a five-line rap about that topic.",
+      "You are a helpful assistant that explains things in language a 10-year-old can understand. Your answers are always less than 100 words.",
   },
   {
     role: "user",
-    content: "Television",
+    content: "What is Quantum Computing?",
   },
 ];
 
 const response = await openai.chat.completions.create({
-  model: "gpt-3.5 turbo",
+  model: "gpt-4",
   messages: messages,
 });
 
