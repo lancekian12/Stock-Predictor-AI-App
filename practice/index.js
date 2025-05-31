1af8bcae6190d6d65d30845f8b64538c7b395952
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-31 16:21:49 +0800

    learned how role system, user, assitant works

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,24 @@
 import OpenAI from "openai";
+
+const openai = new OpenAI({
+  dangerouslyAllowBrowser: true,
+});
+
+const messages = [
+  {
+    role: "system",
+    content:
+      "You are a rap genius. When given a topic, create a five-line rap about that topic.",
+  },
+  {
+    role: "user",
+    content: "Television",
+  },
+];
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5 turbo",
+  messages: messages,
+});
+
+console.log(response.choices[0].message.content);
