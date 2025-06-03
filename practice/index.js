2d9e9bc6d669b8d05a58358cbfdd076d617455aa
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-03 23:41:08 +0800

    learned about stop sequence to limit the output list or lines

--- practice/index.js
+++ practice/index.js
@@ -23,27 +23,49 @@
 
 // console.log(response.choices[0].message.content);
 
-import OpenAI from "openai";
+//! Few shot approach
+// import OpenAI from "openai";
 
+// const messages = [
+//   {
+//     role: "system",
+//     content: `You are a robotic doorman for an expensive hotel. When a customer greets you, respond to them politely. Use examples provided between ### to set the style and tone of your response.`,
+//   },
+//   {
+//     role: "user",
+//     content: `Good day!
+//         ###
+//         Good evening kind Sir. I do hope you are having the most tremendous day and looking forward to an evening of indulgence in our most delightful of restaurants.
+//         ###
+
+//         ###
+//         Good morning Madam. I do hope you have the most fabulous stay with us here at our hotel. Do let me know how I can be of assistance.
+//         ###
+
+//         ###
+//         Good day ladies and gentleman. And isn't it a glorious day? I do hope you have a splendid day enjoying our hospitality.
+//         ### `,
+//   },
+// ];
+
+// const response = await openai.chat.completions.create({
+//   model: "gpt-4",
+//   messages: messages,
+//   max_tokens: 16,
+//   temperature: 1,
+// });
+
+// console.log(response.choices[0].message.content);
+
+import OpenAI from "openai";
 const messages = [
   {
     role: "system",
-    content: `You are a robotic doorman for an expensive hotel. When a customer greets you, respond to them politely. Use examples provided between ### to set the style and tone of your response.`,
+    content: "You are a helpful assistant that knows a lot about books.",
   },
   {
     role: "user",
-    content: `Good day!
-        ###
-        Good evening kind Sir. I do hope you are having the most tremendous day and looking forward to an evening of indulgence in our most delightful of restaurants.
-        ###     
-        
-        ###
-        Good morning Madam. I do hope you have the most fabulous stay with us here at our hotel. Do let me know how I can be of assistance.
-        ###   
-        
-        ###
-        Good day ladies and gentleman. And isn't it a glorious day? I do hope you have a splendid day enjoying our hospitality.
-        ### `,
+    content: "Recommend me some books about learning to code.",
   },
 ];
 
@@ -52,6 +74,7 @@ const response = await openai.chat.completions.create({
   messages: messages,
   max_tokens: 16,
   temperature: 1,
+  stop: ["3."],
 });
 
 console.log(response.choices[0].message.content);
