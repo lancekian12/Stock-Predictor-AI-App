77ef8a6cabe5840c6bbb79c3946ffc442d50d30b
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-03 23:32:25 +0800

    use few shot approach to give examples to ai for controlled output

--- practice/index.js
+++ practice/index.js
@@ -25,19 +25,25 @@
 
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  dangerouslyAllowBrowser: true,
-});
-
 const messages = [
   {
     role: "system",
-    content:
-      "You are a helpful assistant that explains things in language a 10-year-old can understand. Your answers are always less than 100 words.",
+    content: `You are a robotic doorman for an expensive hotel. When a customer greets you, respond to them politely. Use examples provided between ### to set the style and tone of your response.`,
   },
   {
     role: "user",
-    content: "What is Quantum Computing?",
+    content: `Good day!
+        ###
+        Good evening kind Sir. I do hope you are having the most tremendous day and looking forward to an evening of indulgence in our most delightful of restaurants.
+        ###     
+        
+        ###
+        Good morning Madam. I do hope you have the most fabulous stay with us here at our hotel. Do let me know how I can be of assistance.
+        ###   
+        
+        ###
+        Good day ladies and gentleman. And isn't it a glorious day? I do hope you have a splendid day enjoying our hospitality.
+        ### `,
   },
 ];
 
