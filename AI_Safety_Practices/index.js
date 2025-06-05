cbad7f28f4141bb693e65c3a0a995cdc064747ef
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-05 21:18:46 +0800

    added ai safety practice

---
+++ AI_Safety_Practices/index.js
@@ -0,0 +1,29 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  dangerouslyAllowBrowser: true,
+});
+
+async function main() {
+  // Relevant for both inputs and outputs
+  const completion = await openai.moderations.create({
+    input: "I hate you!",
+  });
+  const { flagged, categories } = completion.results[0];
+  console.log("flagged", flagged);
+  console.log("categories", categories);
+
+  //   if (flagged) {
+  //       renderWarning(categories);
+  //   }
+}
+
+main();
+
+function renderWarning(obj) {
+  const keys = Object.keys(obj);
+  const filtered = keys.filter((key) => obj[key]);
+  document.body.innerText = `Your response has been flagged for the following reasons: ${filtered.join(
+    ", "
+  )}.`;
+}
